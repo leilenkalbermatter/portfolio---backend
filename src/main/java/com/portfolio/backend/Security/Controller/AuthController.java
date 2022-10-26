68e9a34485e04d34f929909b87ad5eb2d5312f39
Leilen Kalbermatter <leilenkalbermatter@gmail.com>
2022-10-26 02:41:37 -0300

    cors edit (local)

--- src/main/java/com/portfolio/backend/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/backend/Security/Controller/AuthController.java
@@ -30,8 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://leilenkalbermatter-portf-872a2.web.app")
-//@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "https://leilenkalbermatter-portf-872a2.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
